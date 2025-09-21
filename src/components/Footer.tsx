7a5db01991a1fcec8eee460fc99813976d87ae63
akinolapo <akinolapo@gmail.com>
2025-09-22 00:33:24 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,7 +64,7 @@ const Footer: React.FC = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 Remittra. All rights reserved.</p>
+          <p>&copy; 2025 Remittra. All rights reserved.</p>
         </div>
       </div>
     </footer>
